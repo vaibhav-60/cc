e05d5bc6cf8216db5e755e741e24e02f70e01c93
Vaibhav <vaibhavrai974@gmail.com>
2024-04-11 14:39:53 +0530

    feature-c added

--- server.js
+++ server.js
@@ -8,5 +8,11 @@ function newFeature() {
 function newnewFeature() {
     console.log('bg color wala feature hua dobara');
 }
+function newnewFeatureb() {
+    console.log('bg color wala feature hua dobarab');
+}
+function newnewFeaturec() {
+    console.log('bg color wala feature hua dobarac');
+}
 
 const num = 323;
